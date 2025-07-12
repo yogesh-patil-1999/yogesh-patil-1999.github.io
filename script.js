364ce6c070b1506e719ce7097d37b615eb3c6789
yogesh-patil-1999 <yogpatil.1999.git@gmail.com>
2025-07-12 20:44:41 +0530

    Modal close fix

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
-"use strict";
+'use strict';
 
-const projectText = document.querySelectorAll(".modal");
-const projectName = document.querySelectorAll(".pro-pointer");
-const overlay = document.querySelector(".overlay");
-const btnCloseModal = document.querySelectorAll(".Close-modal");
+const projectText = document.querySelectorAll('.modal');
+const projectName = document.querySelectorAll('.pro-pointer');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelectorAll('.close-modal');
 
 const closeOverlay = function () {
-  overlay.classList.add("hidden");
+  overlay.classList.add('hidden');
 };
 for (let i = 0; i < projectName.length; i++) {
-  projectName[i].addEventListener("click", function () {
-    projectText[i].classList.remove("hidden");
-    overlay.classList.remove("hidden");
+  projectName[i].addEventListener('click', function () {
+    projectText[i].classList.remove('hidden');
+    overlay.classList.remove('hidden');
   });
 }
 
 for (let i = 0; i < projectName.length; i++) {
-  btnCloseModal[i].addEventListener("click", function () {
-    projectText[i].classList.add("hidden");
+  btnCloseModal[i].addEventListener('click', function () {
+    projectText[i].classList.add('hidden');
     closeOverlay();
   });
 }
 
 for (let i = 0; i < projectName.length; i++) {
-  overlay.addEventListener("click", function () {
-    projectText[i].classList.add("hidden");
+  overlay.addEventListener('click', function () {
+    projectText[i].classList.add('hidden');
     closeOverlay();
   });
 }
 
-document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape") {
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
     for (let i = 0; i < projectText.length; i++) {
-      if (!projectText[i].classList.contains("hidden")) {
-        projectText[i].classList.add("hidden");
+      if (!projectText[i].classList.contains('hidden')) {
+        projectText[i].classList.add('hidden');
         closeOverlay();
       }
     }
