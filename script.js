fdbffcf6c6355cf322e6d4b999e579d05c2068a2
yogesh-patil-1999 <yogpatil.1999.git@gmail.com>
2025-07-12 19:13:42 +0530

    UI Website updated

---
+++ script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const projectText = document.querySelectorAll(".modal");
+const projectName = document.querySelectorAll(".pro-pointer");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelectorAll(".Close-modal");
+
+const closeOverlay = function () {
+  overlay.classList.add("hidden");
+};
+for (let i = 0; i < projectName.length; i++) {
+  projectName[i].addEventListener("click", function () {
+    projectText[i].classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+}
+
+for (let i = 0; i < projectName.length; i++) {
+  btnCloseModal[i].addEventListener("click", function () {
+    projectText[i].classList.add("hidden");
+    closeOverlay();
+  });
+}
+
+for (let i = 0; i < projectName.length; i++) {
+  overlay.addEventListener("click", function () {
+    projectText[i].classList.add("hidden");
+    closeOverlay();
+  });
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") {
+    for (let i = 0; i < projectText.length; i++) {
+      if (!projectText[i].classList.contains("hidden")) {
+        projectText[i].classList.add("hidden");
+        closeOverlay();
+      }
+    }
+  }
+});
